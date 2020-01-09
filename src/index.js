82da25a079a4d7c377522ef1e880bfd2e6bfc323
Ricky McConnell <mr.mcconnell12@yahoo.com>
2020-01-09 10:52:50 -0600

    added dummy data on projects using reducers

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
-import { Provider } from "redux-react";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer);
 
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,22 @@
-const initState = {};
+const initState = {
+  projects: [
+    {
+      id: "1",
+      title: "Help find Peach!",
+      content: "Peach needs our help, Mario!"
+    },
+    {
+      id: "2",
+      title: "Collect all the stars",
+      content: "You need 50 stars to face Bowser"
+    },
+    {
+      id: "3",
+      title: "Egg hunt with Yoshi",
+      content: "Yoshi needs help finding all the eggs."
+    }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
