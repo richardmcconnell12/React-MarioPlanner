55ad886bc2a77c5075cff02f6ae4c150abc9ee01
Ricky McConnell <mr.mcconnell12@yahoo.com>
2020-01-15 11:45:40 -0600

    conditionally rendering navbar links pending on login/logout status

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,23 +4,26 @@ import SignInLinks from "./SignInLinks";
 import SignOutLinks from "./SignOutLinks";
 import { connect } from "react-redux";
 
-const NavBar = () => {
+const NavBar = props => {
+  const { auth } = props;
+  // console.log(auth);
+  const links = auth.uid ? <SignInLinks /> : <SignOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
-          MarioPlan
+          Mario Plan
         </Link>
-        <SignInLinks />
-        <SignOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = state => {
-  console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth
+  };
 };
 
 export default connect(mapStateToProps)(NavBar);
