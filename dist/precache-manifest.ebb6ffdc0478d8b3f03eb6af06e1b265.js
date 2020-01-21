a16afabcaebf7bb96e4d539040d1d36c8eb63573
Ricky McConnell <mr.mcconnell12@yahoo.com>
2020-01-21 12:39:13 -0600

    Application deployed to firebase

---
+++ dist/precache-manifest.ebb6ffdc0478d8b3f03eb6af06e1b265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fafa2883577158c4a849f08801410a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e244359af77a0ac15991",
+    "url": "/static/css/main.7e541c36.chunk.css"
+  },
+  {
+    "revision": "7bf2984cca27fa880008",
+    "url": "/static/js/2.bcb17b42.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/static/js/2.bcb17b42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e244359af77a0ac15991",
+    "url": "/static/js/main.7124c12a.chunk.js"
+  },
+  {
+    "revision": "b5456ab03209a6020d17",
+    "url": "/static/js/runtime-main.162f718f.js"
+  }
+]);
